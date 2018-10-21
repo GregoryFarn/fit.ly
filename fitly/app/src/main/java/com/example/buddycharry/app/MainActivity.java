0981dca490338360eef8df7730f7d0c395671b20
Harrison Cha <harrisoc@usc.edu>
2018-10-21 15:16:57 -0700

    Update

--- fitly/app/src/main/java/com/example/buddycharry/app/MainActivity.java
+++ fitly/app/src/main/java/com/example/buddycharry/app/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.buddycharry.app;
+package com.example.a007fa.fitly;
 
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
@@ -8,38 +8,93 @@ import android.text.format.DateFormat;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
+import java.util.BitSet;
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity
-    implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
+        implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
 
-    Button b_pick;
-    TextView tv_result;
+    Button s_pick, e_pick, show;
+    TextView workoutNameView, startTimeView, endTimeView;
+    EditText textData;
 
     int day, month, year, hour, minute;
     int dayFinal, monthFinal, yearFinal, hourFinal, minuteFinal;
+    int counter = 0;
+
+    String time = "";
+
+    Calendar c = Calendar.getInstance();
+    View view;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.addactivity);
 
-        b_pick =  findViewById(R.id.b_pick);
-        tv_result = findViewById(R.id.tv_result);
+        //String myDate = this.time;
+        //Date date = null;
+        //String myDate = "10/18/2018 19:42";
+/*        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm");
 
-        b_pick.setOnClickListener(new View.OnClickListener() {
+        try{
+            date = sdf.parse(myDate);
+        }catch(ParseException e){
+            e.printStackTrace();
+        }*/
+
+
+
+        s_pick = (Button) findViewById(R.id.s_pick);
+        e_pick = (Button) findViewById(R.id.e_pick);
+        show = (Button) findViewById(R.id.show);
+
+        textData = (EditText) findViewById(R.id.workoutName);
+
+        workoutNameView = (TextView) findViewById(R.id.workoutNameView);
+        endTimeView = (TextView) findViewById(R.id.endTimeView) ;
+        startTimeView = (TextView) findViewById(R.id.startTimeView);
+
+        s_pick.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Calendar c = Calendar.getInstance();
                 year = c.get(Calendar.YEAR);
                 month = c.get(Calendar.MONTH);
                 day = c.get(Calendar.DAY_OF_MONTH);
 
                 DatePickerDialog datePickerDialog = new DatePickerDialog(MainActivity.this, MainActivity.this, year, month, day);
                 datePickerDialog.show();
+            }
+        });
+
+        e_pick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                year = c.get(Calendar.YEAR);
+                month = c.get(Calendar.MONTH);
+                day = c.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(MainActivity.this, MainActivity.this, year, month, day);
+                datePickerDialog.show();
+            }
+        });
+
+        show.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String workoutName = textData.getText().toString();
+                String startTime = time.substring(0, time.indexOf('M'));
+                String endTime = time.substring(time.indexOf('M'), time.length());
+                startTime = startTime + "M";
+                endTime = endTime.substring(1, endTime.length());
+                workoutNameView.setText(workoutName);
+                startTimeView.setText(startTime);
+                endTimeView.setText(endTime);
             }
         });
     }
@@ -50,7 +105,8 @@ public class MainActivity extends AppCompatActivity
         monthFinal = month + 1;
         dayFinal = dayOfMonth;
 
-        Calendar c = Calendar.getInstance();
+
+        //Calendar c = Calendar.getInstance();
         hour = c.get(Calendar.HOUR_OF_DAY);
         minute = c.get(Calendar.MINUTE);
 
@@ -62,5 +118,32 @@ public class MainActivity extends AppCompatActivity
     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
         hourFinal = hourOfDay;
         minuteFinal = minute;
+        String period = "N/A", sminute = "N/A", startTime = "N/A", endTime = "N/A";
+
+        if(hourFinal == 0){
+            hourFinal += 12;
+            period = "AM";
+        } else if (hourFinal > 12){
+            hourFinal -= 12;
+            period = "PM";
+        } else if(hourFinal == 12) {
+            period = "PM";
+        } else {
+            period = "AM";
+        }
+
+        if( minuteFinal == 1 || minuteFinal == 2
+                || minuteFinal == 3 || minuteFinal == 4 || minuteFinal == 5 || minuteFinal == 6 || minuteFinal == 7 || minuteFinal == 8 || minuteFinal == 9){
+            sminute = Integer.toString(minute);
+            sminute = "0" + sminute;
+        } else{
+            sminute = Integer.toString(minute);
+        }
+        if(counter == 2){
+            time = "";
+            counter = 0;
+        }
+        time += monthFinal + "/" + dayFinal + "/" + yearFinal + " " + hourFinal + ":" + sminute + ":" + period;
+        counter +=1;
     }
 }
